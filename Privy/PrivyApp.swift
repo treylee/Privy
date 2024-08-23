3f3c866e1ba30ec35679eb4e15c00f765f470817
treylee <trieveon.lee@gmail.com>
2024-08-22 22:40:13 -0400

    Initial Commit

---
+++ Privy/PrivyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrivyApp.swift
+//  Privy
+//
+//  Created by Trieveon Cooper on 8/22/24.
+//
+
+import SwiftUI
+
+@main
+struct PrivyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
